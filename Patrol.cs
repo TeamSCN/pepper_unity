94a01e19a28d3cc5408ae933ba217e4320321904
aiko <aiko.z.1113@gmail.com>
2017-12-22 14:05:12 +0900

    Add coodinate position

--- Patrol.cs
+++ Patrol.cs
@@ -7,13 +7,14 @@ using UnityEngine;
 
 public class Patrol : MonoBehaviour {
     public List<Transform> points;
+    public List<Transform> photo_points;
     private int destPoint = 0;
     private UnityEngine.AI.NavMeshAgent agent;
     private Vector3 startPosition;
     private Vector3 startDirection;
     private Vector3 pastPosition;
     private float pastRotation; //pepperがどっち向いてるか
-    private float pepperSpeed = 2f;
+    private float pepperSpeed = 1f;
     private float timer; //TimerSetにて活動限界を設定
     private bool timeOut = false;
     private String uri = "http://192.168.1.140:8000"; //ここにpepperのURIを指定
@@ -42,7 +43,7 @@ public class Patrol : MonoBehaviour {
 
         // 初期設定
         // autoBrakingをtrueにするとオブジェクトが目的地の前でブレーキをかけるようになる
-        agent.autoBraking = false;
+        agent.autoBraking = true;
         agent.stoppingDistance = 0.2f;
         agent.speed = pepperSpeed;
 
@@ -80,7 +81,6 @@ public class Patrol : MonoBehaviour {
         else if (destPoint + 1 == points.Count + 2)
         {
             Debug.Log("Offset");
-            Debug.Log(agent.transform.position - pastPosition);
             agent.speed = 0f;
             agent.transform.forward = startDirection;
             destPoint++;
@@ -118,20 +118,36 @@ public class Patrol : MonoBehaviour {
         // pepperとagentの距離が小さければGotoNextPoint、大きければGoForIt
         if (agent.remainingDistance < 0.2f && destPoint + 1 <= points.Count + 2)
         {
+            int photo_flg = 0;
+            int stop_flg = 0;
+
             Debug.Log("Reached");
-            Debug.Log(pastRotation - 90);
-            Debug.Log(agent.transform.position - pastPosition);
+            //Debug.Log(pastRotation - 90);
+            //Debug.Log(agent.transform.eulerAngles.y - 90);
+            //Debug.Log(agent.transform.position - pastPosition);
 
             // pepperの座標系に合わせてMoveToへの引数を変換
             double pepperX = (agent.transform.position - pastPosition).x * (Math.Cos((pastRotation - 90) * (Math.PI / 180)))
                 - (agent.transform.position - pastPosition).z * Math.Sin((pastRotation - 90) * (Math.PI / 180));
             double pepperY = (agent.transform.position - pastPosition).x * (Math.Sin((pastRotation - 90) * (Math.PI / 180)))
                 + (agent.transform.position - pastPosition).z * Math.Cos((pastRotation - 90) * (Math.PI / 180));
-            Debug.Log(pepperX + ", " + pepperY);
+            //Debug.Log(pepperX + ", " + pepperY);
 
-            String json = SetJSON(pepperX, pepperY, agent.transform.eulerAngles.y - 90, 0, 1); //0:false, 1:true
+            Debug.Log(destPoint);
+            if (destPoint <= points.Count && photo_points.Contains(points[destPoint-1]))
+            {
+                Debug.Log("say cheese");
+                photo_flg = 1;
+            }
+            if (destPoint == points.Count)
+            {
+                Debug.Log("last one");
+            }
+
+
+            String json = SetJSON(pepperX, pepperY, agent.transform.eulerAngles.y - pastRotation, stop_flg, photo_flg); //0:false, 1:true
             Debug.Log(json);
-            //Send2pepper(json);
+            Send2pepper(json);
 
             pastRotation = agent.transform.eulerAngles.y;
             pastPosition = agent.transform.position;
@@ -168,6 +184,10 @@ public class Patrol : MonoBehaviour {
         else if (destPoint + 1 > points.Count + 2)
         {
             Debug.Log("Everything has been done.");
+            String json = SetJSON(0, 0, agent.transform.eulerAngles.y - pastRotation, 1, 0); //0:false, 1:true
+            Debug.Log(json);
+            //Send2pepper(json);
+
             //TimerSet();
             destPoint = 0;
             Start();
@@ -185,9 +205,23 @@ public class Patrol : MonoBehaviour {
         unityObject.photo_flg = photo_flg;
         string json = JsonUtility.ToJson(unityObject);
         */
-        String json = "{\"0\":{\"x\":" + x + ", \"y\":" + y + ", \"theta\":" + theta + "}, " +
+        String json = "";
+        double rad = (2 * Math.PI * theta / 360) % Math.PI;
+        if (-1 < rad || rad < 1)
+        {
+            json = "{\"0\":{\"x\":" + 0 + ", \"y\":" + 0 + ", \"theta\":" + rad + "}, " +
+            "\"1\":{\"x\":" + Math.Sqrt(x * x + y * y) + ", \"y\":" + 0 + ", \"theta\":" + 0 + "}, " +
             "\"stop_flg\":" + stop_flg + ", \"photo_flg\":" + photo_flg + ", " +
             "\"dest_x\":0.0, \"dest_y\":0.0}";
+        }
+        else
+        {
+            json = "{\"0\":{\"x\":" + 0 + ", \"y\":" + 0 + ", \"theta\":" + rad + "}, " +
+            "\"1\":{\"x\":" + 0 + ", \"y\":" + 0 + ", \"theta\":" + rad*0.2/3.14 + "}, " +
+            "\"2\":{\"x\":" + Math.Sqrt(x * x + y * y) + ", \"y\":" + 0 + ", \"theta\":" + 0 + "}, " +
+            "\"stop_flg\":" + stop_flg + ", \"photo_flg\":" + photo_flg + ", " +
+            "\"dest_x\":0.0, \"dest_y\":0.0}";
+        }
         return json;
     }
 
